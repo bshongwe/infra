179924776582cb8b42790dc5ee6eb3d0399d9667
bshongwe <dru.da.beat.hustle@gmail.com>
2026-03-08 22:52:02 +0200

    feat: add variables for project configuration including project name, environment, AWS region, and Kafka admin credentials -- fix

--- terraform/modules/msk/outputs.tf
+++ terraform/modules/msk/outputs.tf
@@ -1,37 +1,38 @@
-variable "project_name" {
-  description = "Name of the project"
-  type        = string
+output "cluster_arn" {
+  description = "ARN of the MSK cluster"
+  value       = aws_msk_cluster.main.arn
 }
 
-variable "environment" {
-  description = "Environment (dev, staging, prod)"
-  type        = string
-}
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-}
-
-variable "azs" {
-  description = "Availability zones"
-  type        = list(string)
-}
-
-variable "broker_nodes_per_az" {
-  description = "Number of broker nodes per availability zone"
-  type        = number
-  default     = 1
-}
-
-variable "kafka_admin_username" {
-  description = "Kafka admin username for SASL authentication"
-  type        = string
+output "bootstrap_brokers_tls" {
+  description = "TLS bootstrap brokers"
+  value       = aws_msk_cluster.main.bootstrap_brokers_tls
   sensitive   = true
 }
 
-variable "kafka_admin_password" {
-  description = "Kafka admin password for SASL authentication"
-  type        = string
+output "bootstrap_brokers_sasl_iam" {
+  description = "IAM SASL bootstrap brokers"
+  value       = aws_msk_cluster.main.bootstrap_brokers_sasl_iam
   sensitive   = true
+}
+
+output "zookeeper_connect_string" {
+  description = "Zookeeper connection string"
+  value       = aws_msk_cluster.main.zookeeper_connect_string
+  sensitive   = true
+}
+
+output "security_group_id" {
+  description = "Security group ID for MSK cluster"
+  value       = aws_security_group.msk.id
+}
+
+output "topics" {
+  description = "List of created Kafka topics"
+  value = {
+    user_events        = kafka_topic.user_events.name
+    transaction_events = kafka_topic.transaction_events.name
+    auth_events        = kafka_topic.auth_events.name
+    audit_logs         = kafka_topic.audit_logs.name
+    dead_letter_queue  = kafka_topic.dead_letter_queue.name
+  }
 }
